a61944c832fdd55c0a6b2a565648d9e730edc0dd
Christopher Hicks <visrule@protonmail.com>
2022-06-28 15:21:59 -0500

    using classes to show and hide modals : project & handling keypress events

--- Lessons/Projects/Modal-Presentation/script.js
+++ Lessons/Projects/Modal-Presentation/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+// Accessing Classes syntax
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnClose = document.querySelector('.close-modal');
+const btnsOpen = document.querySelectorAll('.show-modal');
+
+const openModalHandler = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModalHandler = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let btnCount = 0; btnCount < btnsOpen.length; btnCount++) {
+  btnsOpen[btnCount].addEventListener('click', openModalHandler);
+}
+
+btnClose.addEventListener('click', closeModalHandler);
+overlay.addEventListener('click', closeModalHandler);
+
+// Add Event Listener to keypresses on keyboard
+// Closes modal when Escape is pressed
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModalHandler();
+  }
+});
+
+//console.log(btnsOpen);
